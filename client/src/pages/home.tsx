f55de56ec0f955a476d487f2cfda14b9e3397a9d
sailesh-ramayanam <ramayanam.sailesh@gmail.com>
2025-11-14 16:12:25 +0530

    Sample change from Stackblitz

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -107,7 +107,7 @@ export default function Home() {
                               Creating...
                             </>
                           ) : (
-                            "Create"
+                            "Create from stackblitz"
                           )}
                         </Button>
                       </div>
